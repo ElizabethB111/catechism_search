d9ee0d5f3ffaa3e1051d4153df4c0a7a1bd3aedc
ElizabethB111 <badereli@bc.edu>
2025-11-08 19:18:20 -0600

    Update header to include logo in Catechism Search

--- catechism_streamlit.py
+++ catechism_streamlit.py
@@ -158,7 +158,10 @@ def hybrid_search(query, encoder, reranker, index, metadata, bm25, top_k=5):
 
 def main():
     # Professional header
-    st.markdown('<h1 class="main-header">✝️ Catechism Search</h1>', unsafe_allow_html=True)
+    st.markdown('<h1 class="main-header">
+        <img src="assets/icons8-catholic-50.png" width="50" style="vertical-align:middle; margin-right:10px;">
+        Catechism Search
+    </h1>', unsafe_allow_html=True)
     st.markdown('<p class="sub-header">A Semantic Study Tool for the Catechism of the Catholic Church</p>', unsafe_allow_html=True)
 
     
